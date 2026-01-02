e39e9859c73a238098e56f2351a3ddd5219586b4
Adrian Gould <Adrian.Gould@nmtafe.wa.edu.au>
2026-01-02 15:55:15 +0800

    fix: Split web routes
    Create separate web.xxx.php route files based on access level
    - web.static.php - general routes
    - web.client.php - authenticated routes
    - web.admin.php - administration routes
    
    web.php now is four lines

--- routes/web.php
+++ routes/web.php
@@ -1,35 +1,10 @@
 <?php
 
-use App\Http\Controllers\Admin\AdminController;
-use App\Http\Controllers\Admin\UserManagementController;
-use App\Http\Controllers\Client\DashboardController;
-use App\Http\Controllers\Client\ProfileController;
-use Illuminate\Support\Facades\Route;
+require __DIR__ . '/web.static.php';
 
-require __DIR__.'/static.php';
+require __DIR__ . '/web.client.php';
 
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('/dashboard', [DashboardController::class, 'dashboard'])
-        ->name('dashboard');
-});
-
-Route::middleware(['auth', 'verified'])
-    ->prefix('admin')
-    ->name('admin.')
-    ->group(function () {
-        Route::get('/', [AdminController::class, 'index'])
-            ->name('index');
-
-        Route::resource('users', UserManagementController::class);
-    });
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])
-        ->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])
-        ->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])
-        ->name('profile.destroy');
-});
+require __DIR__ . '/web.admin.php';
 
 require __DIR__.'/auth.php';
+
